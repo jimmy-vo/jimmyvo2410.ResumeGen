8523cb79956f9f58036b810ea25f621de7c8bc40
duyvo241092 <duyvo241092@users.noreply.github.com>
2018-10-03 13:30:13 -0500

    20181003

--- Content/Template.cs
+++ Content/Template.cs
@@ -9,9 +9,9 @@ namespace API
 {
     public static class Template
     {
-        public static string PathXml = "~/../../../../../jimmyvo2410.github.io/xml/resume.xml";
+        public static string PathXml = "~/../../../../../jimmyvo2410.github.io/xml/profile.xml";
         public static string PathImage = "~/../../../../../jimmyvo2410.github.io/images/";
-        public static string PathOutput = "resume.docx";
+        public static string PathOutput = "~/../../../../../jimmyvo2410.github.io/file/Resume.docx";
 
         public  class Formatt 
         {
